715a8bed46af03f3c11307fe4f52ef23e265b9e1
nathan <nathan@mcminncs.net>
2026-01-31 15:13:04 -0500

    Fixed WPILIB build issues

--- build/spotless/spotlessJava/src/main/java/frc/robot/BuildConstants.java
+++ build/spotless/spotlessJava/src/main/java/frc/robot/BuildConstants.java
@@ -3,15 +3,15 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "AdvantageKit_TalonFXSwerveTemplate";
+  public static final String MAVEN_NAME = "Rebuilt2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = -1;
-  public static final String GIT_SHA = "UNKNOWN";
-  public static final String GIT_DATE = "UNKNOWN";
-  public static final String GIT_BRANCH = "UNKNOWN";
-  public static final String BUILD_DATE = "2026-01-31 13:20:13 EST";
-  public static final long BUILD_UNIX_TIME = 1769883613309L;
-  public static final int DIRTY = 129;
+  public static final int GIT_REVISION = 5;
+  public static final String GIT_SHA = "9d00e1f42ee5ae77df7f59f7172e076223f5728c";
+  public static final String GIT_DATE = "2026-01-31 14:08:24 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-01-31 15:05:28 EST";
+  public static final long BUILD_UNIX_TIME = 1769889928600L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
