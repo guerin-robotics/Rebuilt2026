0c8c78c026d6e6f7102f7cecda785ee5b242bb2a
Garrett Hart <garrett7621@gmail.com>
2026-02-16 15:02:51 -0500

    remove intake canids

--- src/main/java/frc/robot/HardwareConstants.java
+++ src/main/java/frc/robot/HardwareConstants.java
@@ -4,12 +4,7 @@ public class HardwareConstants {
   public static final int NUMBER_OF_CAMERAS = 2;
 
   public static class CanIds {
-    // Intake
-    public static int RIGHT_PIVOT_MOTOR_ID = 20;
-    public static int LEFT_PIVOT_MOTOR_ID = 21;
-    public static int ROLLER_MOTOR_ID = 22;
-
-    // Shooter
+    // Flywheel
     public static int FLYWHEEL_MOTOR_ID = 31;
 
     // Feeder
