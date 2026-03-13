91943277b38721da476cc3ecb9c496bdd82b47d4
SuperBoyIndy <crbrowning63@gmail.com>
2026-03-13 08:33:39 -0400

    Made changes to right auto path

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 87;
-  public static final String GIT_SHA = "09c8b9749c5d4c6bb83e19b38db6b7994c06d87b";
-  public static final String GIT_DATE = "2026-03-11 16:52:56 EDT";
+  public static final int GIT_REVISION = 88;
+  public static final String GIT_SHA = "6309e0f8f07b68f8a8656552151080e1d9b97404";
+  public static final String GIT_DATE = "2026-03-11 16:58:25 EDT";
   public static final String GIT_BRANCH = "autos";
-  public static final String BUILD_DATE = "2026-03-11 16:53:58 EDT";
-  public static final long BUILD_UNIX_TIME = 1773262438302L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-03-11 16:58:45 EDT";
+  public static final long BUILD_UNIX_TIME = 1773262725239L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
