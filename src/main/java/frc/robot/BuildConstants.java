22152eed3fe3470ca6f57ca2beae550cb042c4bd
SuperBoyIndy <crbrowning63@gmail.com>
2026-03-17 20:18:21 -0400

    Added right bump auto

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 134;
-  public static final String GIT_SHA = "f14aaaad7a1220341bc386598b247e20291da5ad";
-  public static final String GIT_DATE = "2026-03-17 19:19:48 EDT";
+  public static final int GIT_REVISION = 135;
+  public static final String GIT_SHA = "a6e6e358c61d219c7dacb1563f68d259890f14c9";
+  public static final String GIT_DATE = "2026-03-17 20:01:58 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-03-17 19:54:31 EDT";
-  public static final long BUILD_UNIX_TIME = 1773791671946L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-03-17 20:05:42 EDT";
+  public static final long BUILD_UNIX_TIME = 1773792342165L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
