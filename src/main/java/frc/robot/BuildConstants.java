0cd288f72bbf26b04cc916b8bad8706b433ce921
ProgrammingLaptop <robotics@guerincatholic.org>
2026-03-22 18:22:26 -0400

    Fixing autos

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 227;
-  public static final String GIT_SHA = "7db163f5b02610ba4dbf4bc3fc9420eee7e31f7b";
-  public static final String GIT_DATE = "2026-03-22 13:40:55 EDT";
+  public static final int GIT_REVISION = 228;
+  public static final String GIT_SHA = "cb30f28b1f9138ba6e26aedd5f7c9d69658e2281";
+  public static final String GIT_DATE = "2026-03-22 16:27:24 EDT";
   public static final String GIT_BRANCH = "Columbus-Competition";
-  public static final String BUILD_DATE = "2026-03-22 16:10:57 EDT";
-  public static final long BUILD_UNIX_TIME = 1774210257696L;
+  public static final String BUILD_DATE = "2026-03-22 17:04:43 EDT";
+  public static final long BUILD_UNIX_TIME = 1774213483009L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
