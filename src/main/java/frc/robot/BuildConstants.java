2d278231438e1e989b5b86ef9a7eae9e5e6e481b
RJ Martin <rjmartin1995@gmail.com>
2026-04-15 13:52:00 -0400

    Back to AM Field in FieldConstants

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 365;
-  public static final String GIT_SHA = "b269bb45fa60e7d85b8dfd087e2b27eb2de18165";
-  public static final String GIT_DATE = "2026-04-15 13:12:55 EDT";
-  public static final String GIT_BRANCH = "fix-vision-jump";
-  public static final String BUILD_DATE = "2026-04-15 13:30:46 EDT";
-  public static final long BUILD_UNIX_TIME = 1776274246628L;
+  public static final int GIT_REVISION = 367;
+  public static final String GIT_SHA = "379d1c7185f489b48272317f3795be173cbff4dc";
+  public static final String GIT_DATE = "2026-04-15 13:31:40 EDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-04-15 13:51:38 EDT";
+  public static final long BUILD_UNIX_TIME = 1776275498470L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
