d58a8e4159a09203653f2d2bb0d744e030d7dc80
RJ Martin <rjmartin1995@gmail.com>
2026-05-17 22:28:52 -0400

    Spotless format build fix

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 474;
-  public static final String GIT_SHA = "dd5677b709061d8163ca269cab819d21333f7d55";
-  public static final String GIT_DATE = "2026-04-30 18:21:29 EDT";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-05-01 14:58:35 EDT";
-  public static final long BUILD_UNIX_TIME = 1777661915633L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 478;
+  public static final String GIT_SHA = "9360da4b16d157be817120111364a6aa357f878c";
+  public static final String GIT_DATE = "2026-05-17 22:27:30 EDT";
+  public static final String GIT_BRANCH = "rj-claude";
+  public static final String BUILD_DATE = "2026-05-17 22:28:27 EDT";
+  public static final long BUILD_UNIX_TIME = 1779071307393L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
