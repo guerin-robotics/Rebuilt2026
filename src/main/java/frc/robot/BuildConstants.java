1363e063614106131779e7c0e0c22c1c5f24400f
ProgrammingLaptop <robotics@guerincatholic.org>
2026-07-01 12:03:15 -0400

    Drive practice

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 495;
-  public static final String GIT_SHA = "7e51458f4f7d488bd0751518e7357219cdf2d808";
-  public static final String GIT_DATE = "2026-06-30 11:17:12 EDT";
+  public static final int GIT_REVISION = 496;
+  public static final String GIT_SHA = "e4765fa524f9543e4fcdb94d87fd127b4921071f";
+  public static final String GIT_DATE = "2026-06-30 12:03:46 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-06-30 11:27:34 EDT";
-  public static final long BUILD_UNIX_TIME = 1782833254557L;
+  public static final String BUILD_DATE = "2026-07-01 09:41:11 EDT";
+  public static final long BUILD_UNIX_TIME = 1782913271145L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
