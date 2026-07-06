dd40c95304a8e9d10fa5b23c75ea600f20fd8161
ProgrammingLaptop <robotics@guerincatholic.org>
2026-07-06 15:38:13 -0400

    upgrade to 26.3.0

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 500;
-  public static final String GIT_SHA = "da3f8fb9a3d3006ed92fe1aff0577f1d8fe764b0";
-  public static final String GIT_DATE = "2026-07-02 07:45:29 EDT";
+  public static final int GIT_REVISION = 506;
+  public static final String GIT_SHA = "b4b8e8c6e9e5dc4881a3b56231525d1b89d8f489";
+  public static final String GIT_DATE = "2026-07-05 21:49:56 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-07-02 10:41:07 EDT";
-  public static final long BUILD_UNIX_TIME = 1783003267033L;
+  public static final String BUILD_DATE = "2026-07-06 15:34:19 EDT";
+  public static final long BUILD_UNIX_TIME = 1783366459012L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
