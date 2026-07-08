a821347d99160c7b9e69e521942f0e174b6ad39f
ProgrammingLaptop <robotics@guerincatholic.org>
2026-07-08 17:27:18 -0400

    Fix shoot timing

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 507;
-  public static final String GIT_SHA = "59c351cef896b3c38b50fb91582c07e0e0d01e1d";
-  public static final String GIT_DATE = "2026-07-06 15:39:44 EDT";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-07-06 18:52:45 EDT";
-  public static final long BUILD_UNIX_TIME = 1783378365134L;
+  public static final int GIT_REVISION = 514;
+  public static final String GIT_SHA = "46f8566bccca16b138af4fe759cddb36fe97445e";
+  public static final String GIT_DATE = "2026-07-08 14:07:47 EDT";
+  public static final String GIT_BRANCH = "auto-IRI";
+  public static final String BUILD_DATE = "2026-07-08 17:23:57 EDT";
+  public static final long BUILD_UNIX_TIME = 1783545837884L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
