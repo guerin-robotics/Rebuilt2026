181d697feb69c13625a068d422492f927fae0af7
RJ Martin <rjmartin1995@gmail.com>
2026-07-24 12:30:17 -0400

    Lowered to measured rate of 4.0 m/s, adjusted in pathplanner

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,15 +3,15 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "Rebuilt2026-1";
+  public static final String MAVEN_NAME = "Rebuilt2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 528;
-  public static final String GIT_SHA = "9fadc35efa43a54861459cd7bf1edeffe428e068";
-  public static final String GIT_DATE = "2026-07-24 11:36:12 EDT";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-07-24 11:36:53 EDT";
-  public static final long BUILD_UNIX_TIME = 1784907413709L;
-  public static final int DIRTY = 0;
+  public static final int GIT_REVISION = 529;
+  public static final String GIT_SHA = "38bcd70fcf63df5ea201c025ef62dc387bbeb28e";
+  public static final String GIT_DATE = "2026-07-24 11:49:20 EDT";
+  public static final String GIT_BRANCH = "top-speed-reduce";
+  public static final String BUILD_DATE = "2026-07-24 12:30:05 EDT";
+  public static final long BUILD_UNIX_TIME = 1784910605044L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
