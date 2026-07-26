ba7bcea191a2acb5f85502a7fd958809819b5bd5
RJ Martin <rjmartin1995@gmail.com>
2026-07-26 00:48:33 -0400

    fix spotless error

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 534;
-  public static final String GIT_SHA = "92401b868ab6ab9f30ea082b9edcd40047b9156e";
-  public static final String GIT_DATE = "2026-07-25 22:56:22 EDT";
+  public static final int GIT_REVISION = 542;
+  public static final String GIT_SHA = "1605210f2617d4c0a26886b6cb6380ddbe312cac";
+  public static final String GIT_DATE = "2026-07-26 00:46:53 EDT";
   public static final String GIT_BRANCH = "feature/drive-slew-rate-limit";
-  public static final String BUILD_DATE = "2026-07-25 23:15:56 EDT";
-  public static final long BUILD_UNIX_TIME = 1785035756498L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-07-26 00:48:21 EDT";
+  public static final long BUILD_UNIX_TIME = 1785041301953L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
