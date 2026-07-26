32757fb6bfb62cff20571de54ac15ea17b7d6116
RJ Martin <rjmartin1995@gmail.com>
2026-07-25 23:30:33 -0400

    Pathplanner gui changes.

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 529;
-  public static final String GIT_SHA = "38bcd70fcf63df5ea201c025ef62dc387bbeb28e";
-  public static final String GIT_DATE = "2026-07-24 11:49:20 EDT";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-07-24 12:22:32 EDT";
-  public static final long BUILD_UNIX_TIME = 1784910152963L;
+  public static final int GIT_REVISION = 534;
+  public static final String GIT_SHA = "92401b868ab6ab9f30ea082b9edcd40047b9156e";
+  public static final String GIT_DATE = "2026-07-25 22:56:22 EDT";
+  public static final String GIT_BRANCH = "feature/drive-slew-rate-limit";
+  public static final String BUILD_DATE = "2026-07-25 23:15:56 EDT";
+  public static final long BUILD_UNIX_TIME = 1785035756498L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
