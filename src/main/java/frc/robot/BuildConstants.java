35c92d6ad9cd33291756d23b07bc9889e4b0b112
RJ Martin <rjmartin1995@gmail.com>
2026-07-26 21:46:24 -0400

    fix accell value

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 534;
-  public static final String GIT_SHA = "e158b4cf40145e840cc27cc12e8a3214d63aa1cb";
-  public static final String GIT_DATE = "2026-07-26 16:56:51 EDT";
+  public static final int GIT_REVISION = 535;
+  public static final String GIT_SHA = "9bf572c2bb5cb7f0513a225b3202df40f9d012fe";
+  public static final String GIT_DATE = "2026-07-26 21:39:10 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-07-26 21:38:57 EDT";
-  public static final long BUILD_UNIX_TIME = 1785116337125L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-07-26 21:40:12 EDT";
+  public static final long BUILD_UNIX_TIME = 1785116412818L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
