6b3eb2054d49ee4c4654752e08a5a53c659e41a0
RJ Martin <rjmartin1995@gmail.com>
2026-07-27 12:03:40 -0400

    fix issue with call of command not needed there

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 536;
-  public static final String GIT_SHA = "35c92d6ad9cd33291756d23b07bc9889e4b0b112";
-  public static final String GIT_DATE = "2026-07-26 21:46:24 EDT";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-07-26 21:46:46 EDT";
-  public static final long BUILD_UNIX_TIME = 1785116806680L;
-  public static final int DIRTY = 0;
+  public static final int GIT_REVISION = 546;
+  public static final String GIT_SHA = "7f08b8fb0bfaef9edaa45b5e3e4d022624d4d426";
+  public static final String GIT_DATE = "2026-07-27 11:44:36 EDT";
+  public static final String GIT_BRANCH = "Intake-Always-Run";
+  public static final String BUILD_DATE = "2026-07-27 12:02:23 EDT";
+  public static final long BUILD_UNIX_TIME = 1785168143495L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
