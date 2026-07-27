e2c37476115d1beb4fbd643e7814d84492ad5b3c
RJ Martin <rjmartin1995@gmail.com>
2026-07-26 21:47:00 -0400

    spotless apply

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 535;
-  public static final String GIT_SHA = "9bf572c2bb5cb7f0513a225b3202df40f9d012fe";
-  public static final String GIT_DATE = "2026-07-26 21:39:10 EDT";
+  public static final int GIT_REVISION = 536;
+  public static final String GIT_SHA = "35c92d6ad9cd33291756d23b07bc9889e4b0b112";
+  public static final String GIT_DATE = "2026-07-26 21:46:24 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-07-26 21:40:12 EDT";
-  public static final long BUILD_UNIX_TIME = 1785116412818L;
+  public static final String BUILD_DATE = "2026-07-26 21:46:46 EDT";
+  public static final long BUILD_UNIX_TIME = 1785116806680L;
   public static final int DIRTY = 0;
 
   private BuildConstants() {}
