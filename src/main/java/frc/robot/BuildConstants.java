9bf572c2bb5cb7f0513a225b3202df40f9d012fe
RJ Martin <rjmartin1995@gmail.com>
2026-07-26 21:39:10 -0400

    Adding initiated exponent value to prevent auto spin

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 533;
-  public static final String GIT_SHA = "ef4d1e30c331009677f0d5ba3089bf9ae081b86c";
-  public static final String GIT_DATE = "2026-07-26 13:55:15 EDT";
-  public static final String GIT_BRANCH = "standardizing-drive-settings";
-  public static final String BUILD_DATE = "2026-07-26 14:24:26 EDT";
-  public static final long BUILD_UNIX_TIME = 1785090266059L;
+  public static final int GIT_REVISION = 534;
+  public static final String GIT_SHA = "e158b4cf40145e840cc27cc12e8a3214d63aa1cb";
+  public static final String GIT_DATE = "2026-07-26 16:56:51 EDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-07-26 21:38:57 EDT";
+  public static final long BUILD_UNIX_TIME = 1785116337125L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
