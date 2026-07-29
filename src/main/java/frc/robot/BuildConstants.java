61f3bc3bb9ed34821fd349396d6bbd8de8f1c218
ProgrammingLaptop <robotics@guerincatholic.org>
2026-07-29 13:57:35 -0400

    have fun fixing merge conflicts. Changed linear velocity and acceleration as well as angular velocity.

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 536;
-  public static final String GIT_SHA = "35c92d6ad9cd33291756d23b07bc9889e4b0b112";
-  public static final String GIT_DATE = "2026-07-26 21:46:24 EDT";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-07-26 21:46:46 EDT";
-  public static final long BUILD_UNIX_TIME = 1785116806680L;
-  public static final int DIRTY = 0;
+  public static final int GIT_REVISION = 540;
+  public static final String GIT_SHA = "b3275db017ea2d8b9eec95e0ab9eb93195807b88";
+  public static final String GIT_DATE = "2026-07-29 10:24:16 EDT";
+  public static final String GIT_BRANCH = "wvrox-auto-changes";
+  public static final String BUILD_DATE = "2026-07-29 13:47:00 EDT";
+  public static final long BUILD_UNIX_TIME = 1785347220068L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
